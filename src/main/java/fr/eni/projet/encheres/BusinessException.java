ed1978fc65c9c9302a8401d903aef761e2d7777a
belin <belin@DESKTOP-OKNLE4N>
2022-07-19 17:10:13 +0200

    MAJ

---
+++ src/main/java/fr/eni/projet/encheres/BusinessException.java
@@ -0,0 +1,5 @@
+package fr.eni.projet.encheres;
+
+public class BusinessException extends Exception {
+
+}
--- src/main/java/fr/eni/projet/encheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/projet/encheres/dal/UtilisateurDAO.java
@@ -1,5 +1,16 @@
 package fr.eni.projet.encheres.dal;
 
+import fr.eni.javaee.module4.BusinessException;
+import fr.eni.projet.encheres.bo.Utilisateur;
+
 public class UtilisateurDAO {
 
+	public interface AvisDAO {
+		/**
+		 * En cas d'erreur, le code d'erreur est enregistré dans l'objet businessException.
+		 * @param utilisateur
+		 * @throws BusinessException
+		 */
+		public void insert(Utilisateur utilisateur) throws BusinessException;
+	}
 }
