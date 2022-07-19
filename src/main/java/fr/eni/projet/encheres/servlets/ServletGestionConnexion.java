5ba577577643633db1fbd597c79476357519a31f
GoulvenENI <109517484+GoulvenENI@users.noreply.github.com>
2022-07-19 14:55:25 +0200

    Update ServletGestionConnexion.java

--- src/main/java/fr/eni/projet/encheres/servlets/ServletGestionConnexion.java
+++ src/main/java/fr/eni/projet/encheres/servlets/ServletGestionConnexion.java
@@ -2,39 +2,23 @@ package fr.eni.projet.encheres.servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletGestionConnexion
- */
 @WebServlet("/ServletGestionConnexion")
 public class ServletGestionConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public ServletGestionConnexion() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/accueil");
+		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
