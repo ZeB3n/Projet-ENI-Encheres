b2522a09dd7fb690efbc274073522994c8cfa7a3
Psylvie <102526281+Psylvie@users.noreply.github.com>
2022-07-19 14:45:12 +0200

    Update Enchere.java

--- src/main/java/fr/eni/projet/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/projet/encheres/bo/Enchere.java
@@ -1,5 +1,60 @@
 package fr.eni.projet.encheres.bo;
 
-public class Enchere {
+import java.util.Date;
 
+public class Enchere {
+	
+	private Date date_enchere;
+	private int montant_enchere;
+	private Article article;
+	private Utilisateur utilisateur;
+	
+	public Enchere() {
+	}
+
+	public Enchere(Date date_enchere, int montant_enchere, Article article, Utilisateur utilisateur) {
+		super();
+		this.date_enchere = date_enchere;
+		this.montant_enchere = montant_enchere;
+		this.article = article;
+		this.utilisateur = utilisateur;
+	}
+
+	public Date getDate_enchere() {
+		return date_enchere;
+	}
+
+	public void setDate_enchere(Date date_enchere) {
+		this.date_enchere = date_enchere;
+	}
+
+	public int getMontant_enchere() {
+		return montant_enchere;
+	}
+
+	public void setMontant_enchere(int montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	@Override
+	public String toString() {
+		return "Enchere [date_enchere=" + date_enchere + ", montant_enchere="
+				+ montant_enchere + ", article=" + article + ", utilisateur=" + utilisateur + "]";
+}
 }
