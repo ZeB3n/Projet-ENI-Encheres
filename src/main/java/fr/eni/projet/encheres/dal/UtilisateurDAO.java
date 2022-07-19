dd5a31f3baaa233450d1eb27bbfa8dcbc1eea823
Gougou <Gougou@DESKTOP-P8BGA4B>
2022-07-19 17:39:17 +0200

    Modif UtilisateurDAO et UtilisateurDAOJdbc

--- src/main/java/fr/eni/projet/encheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/projet/encheres/dal/UtilisateurDAO.java
@@ -3,12 +3,12 @@ package fr.eni.projet.encheres.dal;
 import fr.eni.projet.encheres.BusinessException;
 import fr.eni.projet.encheres.bo.Utilisateur;
 
-public class UtilisateurDAO {
+public interface UtilisateurDAO {
 	/**
 	 * En cas d'erreur, le code d'erreur est enregistré dans l'objet businessException.
 	 * @param utilisateur
 	 * @throws BusinessException
 	 */
-		public void insert(Utilisateur utilisateur) throws BusinessException;
+		public void select(Utilisateur utilisateur) throws BusinessException;
 		
 }
--- src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
+++ src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
@@ -1,5 +1,16 @@
 package fr.eni.projet.encheres.dal.jdbc;
 
-public class UtilisateurDAOJdbc {
+import fr.eni.projet.encheres.BusinessException;
+import fr.eni.projet.encheres.bo.Utilisateur;
+import fr.eni.projet.encheres.dal.UtilisateurDAO;
 
+public class UtilisateurDAOJdbc implements UtilisateurDAO {
+	
+	private static final String SELECT="SELECT INTO utilisateur(pseudo, email, mot_de_passe) VALUES(?,?,?);";
+
+	@Override
+	public void select(Utilisateur utilisateur) throws BusinessException {
+		
+	}
+	
 }
