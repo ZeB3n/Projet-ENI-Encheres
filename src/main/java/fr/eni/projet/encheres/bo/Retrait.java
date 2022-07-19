585d39b2687895fafc9b116726afb7670df34ff2
Psylvie <102526281+Psylvie@users.noreply.github.com>
2022-07-19 14:44:24 +0200

    Update Retrait.java

--- src/main/java/fr/eni/projet/encheres/bo/Retrait.java
+++ src/main/java/fr/eni/projet/encheres/bo/Retrait.java
@@ -1,5 +1,54 @@
 package fr.eni.projet.encheres.bo;
-
 public class Retrait {
+private String rue;
+private String code_postal;
+private String ville;
+private int no_article;
 
+public Retrait() {
+	}
+
+public Retrait(String rue, String code_postal, String ville, int no_article) {
+	super();
+	this.rue = rue;
+	this.code_postal = code_postal;
+	this.ville = ville;
+	this.no_article = no_article;
+}
+
+public String getRue() {
+	return rue;
+}
+
+public void setRue(String rue) {
+	this.rue = rue;
+}
+
+public String getCode_postal() {
+	return code_postal;
+}
+
+public void setCode_postal(String code_postal) {
+	this.code_postal = code_postal;
+}
+
+public String getVille() {
+	return ville;
+}
+
+public void setVille(String ville) {
+	this.ville = ville;
+}
+
+public int getNo_article() {
+	return no_article;
+}
+
+public void setNo_article(int no_article) {
+	this.no_article = no_article;
+}
+public String toString() {
+	return "Retrait [no_article=" + no_article + ", rue=" + rue + ", code_postal=" + code_postal + ", ville="
+			+ ville + "]";
+}
 }
