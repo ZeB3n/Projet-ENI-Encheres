92c1cd3f0a7d25ea95688116c033c2a80f23402f
Gougou <Gougou@DESKTOP-P8BGA4B>
2022-07-20 10:29:52 +0200

    Merge branch 'main' of https://github.com/ZeB3n/Projet-ENI-Encheres.git into main

--- src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
+++ src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
@@ -1,6 +1,7 @@
 package fr.eni.projet.encheres.dal.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,13 +14,28 @@ import fr.eni.projet.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurDAOJdbc implements UtilisateurDAO {
 	
-	private static final String SELECT_CONNEXION_UTILISATEUR="SELECT INTO utilisateur(pseudo, email, mot_de_passe) VALUES(?,?,?);";
+	private static final String SELECT_CONNEXION_UTILISATEUR="SELECT INTO utilisateur(pseudo, mot_de_passe) VALUES(?,?);";
+
 
 	@Override
 	public List<Utilisateur> select() {
-
-		return null;
-		
+		Utilisateur utilisateur = null;
+		PreparedStatement st;
+		try {
+			cnx = JdbcTools.getConnection();
+			st = cnx.prepareStatement(SELECT_CONNEXION_UTILISATEUR);
+			st.setPseudo(1, no_utilisateur);
+			ResultSet rs = st.executeQuery();
+			
+			if (rs.next()) {
+				utilisateur = new Utilisateur();
+				utilisateur.setPseudo(rs.getString(1));
+				utilisateur.setMotDePasse(rs.getString(2));
+			}
+			st.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return utilisateur;
 	}
 }
-
