e3495960d44d9c1187906b8be87c54aa7844e1a1
belin <belin@DESKTOP-OKNLE4N>
2022-07-20 15:04:38 +0200

    Inscription MAJ

--- src/main/java/fr/eni/projet/encheres/servlets/ServletInscription.java
+++ src/main/java/fr/eni/projet/encheres/servlets/ServletInscription.java
@@ -13,12 +13,90 @@ public class ServletInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Affichage de la page d'inscription */
 		getServletContext().getRequestDispatcher("/WEB-INF/jsp/inscription.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		/* Traitement des données du formulaire */
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String code_postal = request.getParameter("code_postal");
+		String ville = request.getParameter("ville");
+		String mot_de_passe = request.getParameter("mot_de_passe");
+		String confirmation = request.getParameter("confirmation");
+		
+		try {
+			validationPseudo(pseudo);
+			validationNom(nom);
+			validationPrenom(prenom);
+			validationEmail(email);
+			validationTelephone(telephone);
+			validationRue(rue);
+			validationCode_Postal(code_postal);
+			validationVille(ville);
+			validationMot_De_Passe(mot_de_passe, confirmation);
+		} catch (Exception e) {
+			/* Gestion des erreurs de validation */
+		}
 	}
+
+	private void validationMot_De_Passe(String mot_de_passe, String confirmation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationVille(String ville) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationCode_Postal(String code_postal) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationRue(String rue) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationTelephone(String telephone) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationEmail(String email) throws Exception {
+		if (email != null && email.trim().length() != 0) {
+			if ( !email.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
+				throw new Exception("Saisir une adresse email valide (pas de @ présent)");
+			}
+		} else {
+			throw new Exception("Le champ email doit être rempli");
+		}
+		
+	}
+
+	private void validationPrenom(String prenom) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationNom(String nom) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void validationPseudo(String pseudo) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
 
--- src/main/webapp/WEB-INF/jsp/inscription.jsp
+++ src/main/webapp/WEB-INF/jsp/inscription.jsp
@@ -11,6 +11,50 @@
 </header>
 <body>
 	<h1>INSCRIPTION</h1>
+	<form method="post" action="inscription">
+            <fieldset>
+                <legend>Inscription</legend>
+                <p>Vous pouvez vous inscrire via ce formulaire.</p>
+                
+                <label for="pseudo"> Pseudo : <span class="requis">*</span></label>
+                <input type="text" id="pseudo" name="pseudo" value="" size="20" maxlength="30" />
+                
+                <label for="nom"> Nom : <span class="requis">*</span></label>
+                <input type="text" id="nom" name="nom" value="" size="20" maxlength="30" />
+                <br />
+                
+                <label for="prenom"> Prénom : <span class="requis">*</span></label>
+                <input type="text" id="prenom" name="prenom" value="" size="20" maxlength="30" />
+
+                <label for="email"> Email <span class="requis">*</span></label>
+                <input type="text" id="email" name="email" value="" size="20" maxlength="50" />
+                <br />
+                
+                <label for="telephone"> Téléphone : </label>
+                <input type="text" id="telephone" name="telephone" value="" size="20" maxlength="10" />
+
+                <label for="rue"> Rue <span class="requis">*</span></label>
+                <input type="text" id="rue" name="rue" value="" size="20" maxlength="30" />
+                <br />
+                
+                <label for="code_postal"> Code Postal : <span class="requis">*</span></label>
+                <input type="text" id="code_postal" name="code_postal" value="" size="20" maxlength="30" />
+
+                <label for="ville"> Ville : <span class="requis">*</span></label>
+                <input type="text" id="ville" name="ville" value="" size="20" maxlength="30" />
+                <br />
+
+                <label for="motdepasse"> Mot de passe : <span class="requis">*</span></label>
+                <input type="password" id="mot_de_passe" name="mot_de_passe" value="" size="20" maxlength="20" />
+
+                <label for="confirmation"> Confirmation : <span class="requis">*</span></label>
+                <input type="password" id="confirmation" name="confirmation" value="" size="20" maxlength="20" />
+                <br />
+
+                <input type="submit" value="Créer" class="sansLabel" />
+                <br />
+            </fieldset>
+        </form>
 		<a href="<%=request.getContextPath()%>/ServletGestionConnexion"><button type="submit" formmethod="post">Créer un compte</button></a>
 		<a href="<%=request.getContextPath()%>/ServletAccueil"><button type="submit">Annuler</button></a>
 </body>
