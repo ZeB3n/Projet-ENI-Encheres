cd453c4802c971003ebb7a880297bb49a3f4b0e2
Gougou <Gougou@DESKTOP-P8BGA4B>
2022-07-20 10:01:49 +0200

    Maj UtilisateurDAO et UtilisateurDAOJdbc

--- src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
+++ src/main/java/fr/eni/projet/encheres/dal/jdbc/UtilisateurDAOJdbc.java
@@ -1,17 +1,25 @@
 package fr.eni.projet.encheres.dal.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.eni.projet.encheres.BusinessException;
 import fr.eni.projet.encheres.bo.Utilisateur;
 import fr.eni.projet.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurDAOJdbc implements UtilisateurDAO {
 	
-	private static final String SELECT="SELECT INTO utilisateur(pseudo, email, mot_de_passe) VALUES(?,?,?);";
+	private static final String SELECT_CONNEXION_UTILISATEUR="SELECT INTO utilisateur(pseudo, email, mot_de_passe) VALUES(?,?,?);";
 
 	@Override
-	public void select(Utilisateur utilisateur) throws BusinessException {
+	public List<Utilisateur> select() {
+
+		return null;
 		
 	}
-	
 }
 
