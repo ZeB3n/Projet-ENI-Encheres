7f8cad6884a503e1323ca815b8438b9a3b520e68
Gougou <Gougou@DESKTOP-P8BGA4B>
2022-07-28 10:18:30 +0200

    Modif ServletAccueil

--- src/main/java/fr/eni/projet/encheres/servlets/ServletAccueil.java
+++ src/main/java/fr/eni/projet/encheres/servlets/ServletAccueil.java
@@ -10,10 +10,10 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ServletAccueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public static final String VUE = "/WEB-INF/jsp/accueil.jsp";
+	public static final String ACCUEIL = "/WEB-INF/jsp/accueil.jsp";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher( VUE ).forward(request, response);
+		this.getServletContext().getRequestDispatcher( ACCUEIL ).forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
